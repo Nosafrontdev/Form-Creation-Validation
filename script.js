266dcbde1b5034c9ad9411620411e579eb079527
Nosafrontdev <nosyblink@gmail.com>
2025-08-03 20:54:59 +0100

    feature of adding event lsitener

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+const form = document.getElementById("Registration-form");
+
+const feedbackDiv = document.getElementById("form-feedback");
+
+form.addEventListener('submit', function(event)){
+    event.preventDefault();
+}
+
+
+
+
+
 }

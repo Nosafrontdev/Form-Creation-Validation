293365788d14f96dd5d7542689800fcc26a4b9fc
Nosafrontdev <nosyblink@gmail.com>
2025-08-03 21:09:47 +0100

    correct registration spelling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-const form = document.getElementById("Registration-form");
+const form = document.getElementById("registration-form");
 
 const feedbackDiv = document.getElementById("form-feedback");
 

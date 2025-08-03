e0dc8f3fb30b64aba254e1759c46e13e3b5d05cd
Nosafrontdev <nosyblink@gmail.com>
2025-08-03 21:07:27 +0100

    added getting username, email, password

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const form = document.getElementById("Registration-form");
 
 const feedbackDiv = document.getElementById("form-feedback");
 
-form.addEventListener('submit', function(event)){
-    event.preventDefault();
+  form.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
 }
 
 
